3b9ba7340c106cb387460b44fea1a84a914ceb09
Egemen Kalyoncu <ekalyoncu@marktplaats.nl>
2015-05-08 09:39:08 +0200

    fixy

--- init.sql
+++ init.sql
@@ -33,6 +33,6 @@ INSERT INTO item (id, title, photo, description) VALUES (6, '6-persoons Zilver B
 INSERT INTO item (id, title, photo, description) VALUES (7, 'Racing bike', '/img/bike1.jpeg', 'Looks cool, rides smooth');
 INSERT INTO item (id, title, photo, description) VALUES (8, 'Ray Ban zonnebril', 'http://i.marktplaats.com/00/s/NTAwWDY2Nw==/z/WWAAAOSwEeFVS2TM/$_85.JPG', 'wayfare');
 INSERT INTO item (id, title, photo, description) VALUES (9, 'Stoffen 2 zitter bank', 'http://i.marktplaats.com/00/s/NTQ1WDcyNg==/z/ZP4AAOSwNSxVS3lc/$_84.JPG', '1,50 breed, 65 hoog, 85 diep');
-INSERT INTO item (id, title, photo, description) VALUES (10, 'Witte retro vloerlamp', '/img/lamp1.jpeg', '1.5m hoog, verstelbaar');
+INSERT INTO item (id, title, photo, description) VALUES (10, 'Witte retro vloerlamp', '/img/lamp1.jpg', '1.5m hoog, verstelbaar');
 INSERT INTO item (id, title, photo, description) VALUES (11, 'Gazelle Madeo fiets', 'http://i.marktplaats.com/00/s/NDUwWDgwMA==/z/S0YAAOSwstxVQks4/$_85.JPG', '54cm frame, 24 gears, with lock');
 INSERT INTO item (id, title, photo, description) VALUES (12, 'Babboe Big Bakfiets', 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/I84AAOSwPhdVLpGn/$_85.JPG', '5 gears, for 4 kids');
